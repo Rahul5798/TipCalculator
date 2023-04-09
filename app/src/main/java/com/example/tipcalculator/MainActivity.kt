0bfa1c23ebeb3873df82e8fd17e528252300fcb1
Rahul Patel <63693793+Rahul5798@users.noreply.github.com>
2023-04-09 15:27:13 -0400

    Tip percent field added and working

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -3,17 +3,21 @@ package com.example.tipcalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -37,24 +41,62 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TipTimeScreen(modifier : Modifier = Modifier){
+    //Focus manager to change focus as per keyboard's action button
+    val focusManager = LocalFocusManager.current
+    //Variables used for getting value from first text field
     var amountInput by remember {
         mutableStateOf("")
     }
+
     val amount = amountInput.toDoubleOrNull() ?: 0.0
-    val tip = calculateTip(amount)
+    //Variables used for getting value from second text field
+    var tipPercentInput by remember {
+        mutableStateOf("")
+    }
+    val tipPercent = tipPercentInput.toDoubleOrNull() ?: 0.0
+    //calculating tip
+    val tip = calculateTip(amount, tipPercent)
+    //Column layout
     Column(modifier = Modifier
         .padding(32.dp)
         .fillMaxSize(),
         verticalArrangement = Arrangement.spacedBy(8.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        //Title
         Text(
             text = stringResource(id = R.string.calculate_tip),
             fontSize = 24.sp,
             modifier = Modifier.align(Alignment.CenterHorizontally)
         )
+        //Space between title and first text field
         Spacer(modifier = Modifier.height(16.dp))
-        EditNumberField(value = amountInput, onValueChange = { amountInput = it})
+        //First text field to enter number with keyboard type number and action button as next
+        EditNumberField(labelId = R.string.bill_amount,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(onNext = {
+                focusManager.moveFocus(FocusDirection.Down)
+            }),
+            value = amountInput,
+            onValueChange = { amountInput = it})
+        //Space between two text fields
+        Spacer(modifier = Modifier.height(16.dp))
+        //Second text field to enter number with keyboard type number and action button as done
+        EditNumberField(labelId = R.string.how_was_the_service,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(onDone = {
+                focusManager.clearFocus()
+            }),
+            value = tipPercentInput,
+            onValueChange = { tipPercentInput = it}
+        )
+
         Spacer(modifier = Modifier.height(24.dp))
         Text(text = stringResource(id = R.string.tip_amount,tip),
         modifier = Modifier.align(Alignment.CenterHorizontally),
@@ -67,18 +109,23 @@ fun TipTimeScreen(modifier : Modifier = Modifier){
 }
 
 @Composable
-fun EditNumberField(value : String, onValueChange : (String) -> Unit){
-
+fun EditNumberField(@StringRes labelId : Int,
+                    keyboardOptions: KeyboardOptions,
+                    keyboardActions: KeyboardActions,
+                    value : String,
+                    onValueChange : (String) -> Unit){
     TextField(
-        label = {Text(text = stringResource(id = R.string.cost_of_service))},
+        label = {Text(text = stringResource(labelId))},
         value = value,
         onValueChange = onValueChange,
         singleLine = true,
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions,
         modifier = Modifier.fillMaxWidth()
     )
 }
-private fun calculateTip(amount : Double, tipPercent : Double = 15.0): String {
+
+private fun calculateTip(amount : Double, tipPercent : Double): String {
     val tip =tipPercent/100 * amount
     return NumberFormat.getCurrencyInstance().format(tip)
 }
