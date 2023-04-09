2d69950888ab496e85296c03c22b764a728a1eed
Rahul Patel <63693793+Rahul5798@users.noreply.github.com>
2023-04-09 12:26:04 -0400

    Screen Title and number text field created

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.tipcalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.tipcalculator.ui.theme.TipCalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +22,41 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TipCalculatorTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Surface(){
+                    TipTimeScreen()
                 }
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun TipTimeScreen(modifier : Modifier = Modifier){
+    Column(modifier = Modifier.padding(32.dp),
+    verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        Text(
+            text = stringResource(id = R.string.calculate_tip),
+            fontSize = 24.sp,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        EditNumberField()
+        
+    }
 
+}
+@Composable
+fun EditNumberField(modifier: Modifier = Modifier){
+    TextField(
+        value = "",
+        onValueChange = {}
+    )
+}
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    TipCalculatorTheme {
-        Greeting("Android")
+fun DefaultPreview(){
+    TipCalculatorTheme() {
+        TipTimeScreen()
     }
 }
